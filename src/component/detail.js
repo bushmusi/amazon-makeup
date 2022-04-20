6a3c99714dbd2d13787ef86857eccaae1414e2c9
bushmusi <bushera.mestofa@gmail.com>
2022-04-20 16:36:16 +0300

    Add detail page

--- src/component/detail.js
+++ src/component/detail.js
@@ -1,9 +1,69 @@
+import { MdKeyboardBackspace } from 'react-icons/md';
+import { useNavigate  } from 'react-router-dom';
 export default function Detail() {
+  const navigate = useNavigate();
     return (
-<template>
-  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-    <slot/>
-  </button>
-</template>
+      <>
+      <div className="flex p-5 items-center">
+        <span to="home" className='text-white flex-1 cursor-pointer' onClick={()=> navigate(-1)}><MdKeyboardBackspace size={30}/> </span>
+        <h1 className="text-white">Category Lists</h1>
+      </div>
+      <ul className="grid grid-cols-3 gap-5 justify-center my-8 p-10">
+        <li className="rounded-lg border-4 border-sky p-3"  style={{ backgroundColor: '#00000040' }}>
+          <div className="flex justify-center">
+            <img src="https://d3t32hsnjxo7q6.cloudfront.net/i/2d46e82f21b11f658a4378abcbd1c31b_ra,w158,h184_pa,w158,h184.png"/>
+          </div>
+          <div>
+            <ul className="p-5">
+              <li className="text-white"><b className="text-gray-300">Brand:</b> covergirl</li>
+              <li className="text-white"><b className="text-gray-300">Product Type:</b> lipstick</li>
+              <li className="text-white"><b className="text-gray-300">Name:</b> CoverGirl Outlast Longwear Lipstick</li>
+            </ul>
+            <button type="button" className="w-full bg-white h-10 text-gray-500">Add To Cart $10.99</button>
+          </div>
+        </li>
+
+        <li className="rounded-lg border-4 border-sky p-3"  style={{ backgroundColor: '#00000040' }}>
+          <div className="flex justify-center">
+            <img src="https://d3t32hsnjxo7q6.cloudfront.net/i/2d46e82f21b11f658a4378abcbd1c31b_ra,w158,h184_pa,w158,h184.png"/>
+          </div>
+          <div>
+            <ul className="p-5">
+              <li className="text-white"><b className="text-gray-300">Brand:</b> covergirl</li>
+              <li className="text-white"><b className="text-gray-300">Product Type:</b> lipstick</li>
+              <li className="text-white"><b className="text-gray-300">Name:</b> CoverGirl Outlast Longwear Lipstick</li>
+            </ul>
+            <button type="button" className="w-full bg-white h-10">Add To Cart $10.99</button>
+          </div>
+        </li>
+
+        <li className="rounded-lg border-4 border-sky p-3"  style={{ backgroundColor: '#00000040' }}>
+          <div className="flex justify-center">
+            <img src="https://d3t32hsnjxo7q6.cloudfront.net/i/2d46e82f21b11f658a4378abcbd1c31b_ra,w158,h184_pa,w158,h184.png"/>
+          </div>
+          <div>
+            <ul className="p-5">
+              <li className="text-white"><b className="text-gray-300">Brand:</b> covergirl</li>
+              <li className="text-white"><b className="text-gray-300">Product Type:</b> lipstick</li>
+              <li className="text-white"><b className="text-gray-300">Name:</b> CoverGirl Outlast Longwear Lipstick</li>
+            </ul>
+            <button type="button" className="w-full bg-white h-10">Add To Cart $10.99</button>
+          </div>
+        </li>
+        <li className="rounded-lg border-4 border-sky p-3"  style={{ backgroundColor: '#00000040' }}>
+          <div className="flex justify-center">
+            <img src="https://d3t32hsnjxo7q6.cloudfront.net/i/2d46e82f21b11f658a4378abcbd1c31b_ra,w158,h184_pa,w158,h184.png"/>
+          </div>
+          <div>
+            <ul className="p-5">
+              <li className="text-white"><b className="text-gray-300">Brand:</b> covergirl</li>
+              <li className="text-white"><b className="text-gray-300">Product Type:</b> lipstick</li>
+              <li className="text-white"><b className="text-gray-300">Name:</b> CoverGirl Outlast Longwear Lipstick</li>
+            </ul>
+            <button type="button" className="w-full bg-white h-10">Add To Cart $10.99</button>
+          </div>
+        </li>
+      </ul>
+      </>
     )
 }
