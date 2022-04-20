e3a676e416dc71b8f0fd2c34e2509724cba1dd24
bushmusi <bushera.mestofa@gmail.com>
2022-04-20 16:36:30 +0300

    Add content to home

--- src/component/home.js
+++ src/component/home.js
@@ -1,27 +1,33 @@
 
+import {MdArrowForward} from 'react-icons/md';
+import { NavLink, Outlet  } from 'react-router-dom';
+
+const catagory = ['blush','bronzer','eyebrow','eyeliner','foundation','lip_liner','lipstick','nail_polish']
+
 export default function Home() {
     return (
-        <table className="border-collapse border border-slate-400 ...">
-        <thead>
-            <tr>
-            <th className="border border-slate-300 ...">State</th>
-            <th className="border border-slate-300 ...">City</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-            <td className="border border-slate-300 ...">Indiana</td>
-            <td className="border border-slate-300 ...">Indianapolis</td>
-            </tr>
-            <tr>
-            <td className="border border-slate-300 ...">Ohio</td>
-            <td className="border border-slate-300 ...">Columbus</td>
-            </tr>
-            <tr>
-            <td className="border border-slate-300 ...">Michigan</td>
-            <td className="border border-slate-300 ...">Detroit</td>
-            </tr>
-        </tbody>
-        </table>
+        <div className="p-5">
+            <div className='flex justify-center p-5'>
+                <form className='w-1/3'>
+                    <input type="text" placeholder="Search by catagory type" className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" name="search"/>
+                </form>
+            </div>
+            <ul className="grid gap-x-8 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-5 mb-20 ">
+                {
+                    catagory.map((item,index) => (
+                        <li style={{ backgroundColor: '#00000040' }}  key={`${item}${index}`} className="max-w-sm rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 p-5 cursor-pointer">
+                            <NavLink to={`detail?product_type=${item}`} className="">
+                                <div href="#" className="flex">
+                                    <img className="p-8 rounded-t-lg color-white w-64 flex-1" src={require(`../assets/${item}.svg`)} alt="product image" />
+                                    <p className='text-white'><MdArrowForward /></p>
+                                </div>
+                                <h3 className='text-center font-medium leading-8 text-white'>{item}</h3>
+                            </NavLink>
+                            <Outlet />
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
     )
 }
